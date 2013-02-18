2efce7277f2bc838c6fd3132efa9a479de1d6171
qianlifeng <qianlf2008@163.com>
2013-02-18 13:53:24 +0800

    add most favorite tab

--- exshanbay/scripts/CS_main.js
+++ exshanbay/scripts/CS_main.js
@@ -18,9 +18,9 @@ injectScript = function() {
                 var review_detail_view = new ReviewDetailView({
                     model: this.model
                 });
-                inject();
                 review_detail_view.bind('next', this.next, this);
                 review_detail_view.bind('prev', this.prev, this);
+                inject(this.model.attributes["content_id"]);
         };
     }
     else
@@ -28,7 +28,7 @@ injectScript = function() {
         console.log("exshanbay:could not find review view");
     }
 
-    function inject(){
+    function inject(vocabularyId){
         $(".note-user-box-tab").click();
         //添加标签
         var tabTitle = '最喜欢';
@@ -37,7 +37,7 @@ injectScript = function() {
         var wrapper = '<div class="tab-pane row" id="ex-favorite-box"><ol class="span9"></ol></div>';
         $(".tab-content").append(wrapper);
         $(".ex-favorite-box-tab").click();
-        $("#ex-favorite-box ol").append(getMostFavorite());
+        getMostFavorite(vocabularyId,function(list){$("#ex-favorite-box ol").append(list)});
     }
     
 
@@ -46,7 +46,7 @@ injectScript = function() {
      *
      * @return 最喜欢的例句html 
      */
-    function getMostFavorite(){
+    function getMostFavorite(vocabularyId,callBack){
         var list = [];
         $("#ex-defaults-box .like").each(function(){
             var patrn= /喜欢（(\d+)）/ig;
@@ -56,13 +56,34 @@ injectScript = function() {
             }
         });
 
-        //排序
-        list.sort(function(a,b){return b[0] - a[0];});
-        
-        var returnStr = "";
-        for (var i = 0; i < list.length; i += 1) {
-            returnStr += $('<a>').append(list[i][1].clone()).html(); 
-        };
-        return returnStr;
+        $.ajax("http://www.shanbay.com/api/v1/example/?vocabulary_id="+vocabularyId).done(function(d){
+            for(var index in d.objects) {
+                var i = d.objects[index];
+                var html = '<li class="row">'+
+                            '<div class="span9"><div class="index pull-left"></div><div class="pull-left body">'+
+                                '<div class="enex">'+i.first+'<span class="mid">'+i.mid+'</span>'+i.last+'</div>'+
+                                '<div class="cnex">'+i.translation+'</div>'+
+                                '<div class="edit-example-box"></div></div><div class="actions-wrap"><div class="actions hide btn-group pull-right">'+
+                                    '<a class="like btn btn-mini" href="javascript:void(0)">喜欢（'+i.likes+'）</a>'+
+                                    '<a class="unlike btn btn-mini" href="javascript:void(0)">不喜欢（'+i.unlikes+'）</a>'+
+                                    '<a class="more hide btn btn-mini" href="javascript:void(0)">更多</a></div></div>'+
+                            '</div>'+
+                           '</li>';
+                list.push([i.likes,$(html)]);
+            }
+            //排序
+            list.sort(function(a,b){return b[0] - a[0];});
+            
+            var returnStr = "";
+            var showCount = 5;
+            if(list.length < showCount) {
+                showCount = list.length;
+            }
+            for (var i = 0; i < showCount; i += 1) {
+                returnStr += $('<a>').append(list[i][1].clone()).html(); 
+            };
+
+            callBack(returnStr);
+        });
     }
 }
